b93b793ca94d5ec4ab823dbb9c95efe5bda7727d
HowdyHowe <amiruddinstr@gmail.com>
2025-09-25 19:35:44 +0700

    Installing prisma for db model.

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,16 +1,13 @@
 import { Request, Response } from "express";
-import pool from "../config/db";
 
-export const userSignup = (req: Request, res: Response) => {
-    try{
-        const { username, password } = req.body;
-        res.json({ message: "User Signed up", data: { username, password } })
-    } catch (err) {
-        console.error(err)
-    }
+export const getAll = async (req: Request, res: Response) => {
+
+}
+
+export const userSignup = async (req: Request, res: Response) => {
+
 };
 
 export const userLogin = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    res.json({ message: "User logged in", data: username })
+
 }
--- src/routing/authRoutes.ts
+++ src/routing/authRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { userSignup, userLogin } from "../controllers/authControllers";
+import { userSignup, userLogin, getAll } from "../controllers/authControllers";
 
 const router = Router();
 
 router.post("/signup", userSignup);
 router.post("/login", userLogin)
+router.get("/getall", getAll)
 
 export default router;

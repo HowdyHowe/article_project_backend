09352ab8d3c8d632c071af9edc365fdc2b16251e
HowdyHowe <amiruddinstr@gmail.com>
2025-09-26 08:41:25 +0700

    Installing bcrypt and using it for password hashing.

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,7 +1,7 @@
 import z from "zod";
-import { Request, Response } from "express";
-import prisma from "../prisma/client";
+import bcrypt from "bcrypt";
 import * as userModel from "../models/authModel";
+import { Request, Response } from "express";
 import { generateUserId } from "../utils/generateID";
 import { sendResponse } from "../utils/response";
 
@@ -23,33 +23,47 @@ export const getAll = async (req: Request, res: Response) => {
 
 export const userSignup = async (req: Request, res: Response) => {
     try {
-        const { username, password } = authSchema.parse(req.body)
+        const { username, password } = authSchema.parse({
+            username: req.body.username?.trim(),
+            password: req.body.password?.trim()
+        })
         const user_id = await generateUserId();
-        const newUser = await userModel.createUser({user_id, username, password});
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await userModel.createUser({user_id: user_id, username: username, password: hashedPassword});
 
-        return res.status(201).json({message: "Successfully created", statusCode: 201, data: {user_id: newUser.user_id, username: newUser.username}});
+        return sendResponse(res, 201, "Successfully Created", { user_id: newUser.user_id, username: newUser.username  });
     } catch (err: any) {
-        if (err.code === "P2002") return res.status(409).json({message: "Username already exist", statusCode: 409})
         if (err instanceof z.ZodError) {
             const messages = err.issues.map((e) => e.message);
-            return res.status(400).json({message: messages, statusCode: 400})
+            return sendResponse(res, 400, messages.join(", "));
         }
+        if (err.code === "P2002") return sendResponse(res, 409, "Username already exist");
 
-        res.status(500).json({message: "Something went wrong", statusCode: 500})
-        console.error(err)
+        console.error(err);
+        return sendResponse(res, 500, "Something went wrong");
     }
 };
 
 export const userLogin = async (req: Request, res: Response) => {
     try {
-        const { username, password } = req.body
+        const { username, password } = authSchema.parse({
+            username: req.body.username?.trim(),
+            password: req.body.password?.trim()
+        })
         const user = await userModel.getUser(username);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-        if (!user) return res.status(401).json({message: "Invalid username or password"})
-        if (user.password !== password) return res.status(401).json({message: "Invalid username or password", statusCode: 401})
+        if (!user) return sendResponse(res, 401, "Invalid username or password");
+        if (user.password !== hashedPassword) return sendResponse(res, 401, "Invalid username or password");
 
-        res.status(200).json({message: "Successfully logged in", statusCode: 200})
+        return sendResponse(res, 200, "Successfully logged in", { user_id: user.user_id, username: user.username });
     } catch (err: any) {
-        console.error(err)
+        if (err instanceof z.ZodError) {
+            const messages = err.issues.map((e) => e.message);
+            return sendResponse(res, 400, messages.join(", "));
+        }
+
+        console.error(err);
+        return sendResponse(res, 500, "Something went wrong");
     }
 }
